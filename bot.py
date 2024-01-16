f580e2164570fd34e417a22b654f115f66ff6508
Keylz <nekozux@gmail.com>
2024-01-16 00:01:28 -1100

    Update bot.py

--- bot.py
+++ bot.py
@@ -144,4 +144,10 @@ def handle_message(message):
         print(f"Answer Text: {answer_text}")
         print(f"Text: {text}")
 
-bot.infinity_polling()
+try:
+    bot.infinity_polling(timeout=10, long_polling_timeout=5)
+except (ConnectionError, ReadTimeout) as e:
+    sys.stdout.flush()
+    os.execv(sys.argv[0], sys.argv)
+else:
+    bot.infinity_polling(timeout=10, long_polling_timeout=5)
