33fcd89f47eb288af9438e2019aa4006a02ff0be
Keylz <nekozux@gmail.com>
2024-01-19 05:09:30 -1100

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ import datetime
 from requests.exceptions import ConnectionError, ReadTimeout
 
 # Inisialisasi bot Telegram
-bot_token = '6453852810:AAHrni3aOZaD27DI4Mwmxo7T2F5W06IWaRU'
+bot_token = '6453852810:AAGyN86AZW7VCgi7KtYiDzwO-RuyG9qyURE'
 bot = telebot.TeleBot(bot_token)
 
 @bot.message_handler(commands=['start'])
